418f8110a95aeaaba11a3a04e844a329d5de9c43
Collins Tatang <makungongcollins@gmail.com>
2021-07-05 20:54:46 +0100

    fix linters

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let collection = JSON.parse(localStorage.getItem('collection')) || [];
-
-const add = (title, author_name) => {
-    const book = { title, author_name };
+const add = (title, author) => {
+    const book = { title, author };
     collection.push(book);
     localStorage.setItem('collection', JSON.stringify(collection));
 };
@@ -11,23 +10,21 @@ const remove = (title) => {
     localStorage.setItem('collection', JSON.stringify(collection));
 };
 
-
 const createBookDiv = (book) => {
     const bookDiv = document.createElement('div');
     bookDiv.setAttribute('class', 'bookDiv');
     const title = document.createElement('p');
     title.textContent = book.title;
-    const author_name = document.createElement('p');
-    author_name.textContent = book.author_name;
+    const author = document.createElement('p');
+    author.textContent = book.author;
     const delBtn = document.createElement('button');
     delBtn.setAttribute('id', book.title);
     delBtn.textContent = 'Remove';
     delBtn.setAttribute('class', 'delete');
     const hr = document.createElement('hr');
-    bookDiv.append(title, author_name, delBtn, hr);
+    bookDiv.append(title, author, delBtn, hr);
     return bookDiv;
 };
-
 
 const showcollection = () => {
     const bookList = document.querySelector('#collection-list');
@@ -41,14 +38,14 @@ const showcollection = () => {
 
 const clearInputs = () => {
     document.querySelector('#title').value = '';
-    document.querySelector('#author_name').value = '';
+    document.querySelector('#author').value = '';
 };
 
 document.querySelector('#book-form').addEventListener('submit', (event) => {
     event.preventDefault();
     const title = document.querySelector('#title').value;
-    const author_name = document.querySelector('#author_name').value;
-    add(title, author_name);
+    const author = document.querySelector('#author').value;
+    add(title, author);
     showcollection();
     clearInputs();
 });
