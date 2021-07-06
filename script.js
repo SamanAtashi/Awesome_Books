5729acb0538fe21a2c008cdea8c7a7da06ebabf6
Collins Tatang <makungongcollins@gmail.com>
2021-07-06 15:37:04 +0100

    Refactor books into Book class

--- script.js
+++ script.js
@@ -1,61 +1,70 @@
-let collection = JSON.parse(localStorage.getItem('collection')) || [];
-const add = (title, author) => {
-  const book = { title, author };
-  collection.push(book);
-  localStorage.setItem('collection', JSON.stringify(collection));
-};
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
 
-const remove = (title) => {
-  collection = collection.filter((book) => book.title !== title);
-  localStorage.setItem('collection', JSON.stringify(collection));
-};
+    static books = JSON.parse(localStorage.getItem('books')) || [];
+
+    static addBook(book) {
+        Book.books.push(book);
+        localStorage.setItem('books', JSON.stringify(Book.books));
+    }
+
+    static removeBook(title) {
+        Book.books = Book.books.filter((book) => book.title !== title);
+        localStorage.setItem('books', JSON.stringify(Book.books));
+    }
+}
 
 const createBookDiv = (book) => {
-  const bookDiv = document.createElement('div');
-  bookDiv.setAttribute('class', 'bookDiv');
-  const title = document.createElement('p');
-  title.textContent = book.title;
-  const author = document.createElement('p');
-  author.textContent = book.author;
-  const delBtn = document.createElement('button');
-  delBtn.setAttribute('id', book.title);
-  delBtn.textContent = 'Remove';
-  delBtn.setAttribute('class', 'delete');
-  const hr = document.createElement('hr');
-  bookDiv.append(title, author, delBtn, hr);
-  return bookDiv;
+    const bookDiv = document.createElement('div');
+    bookDiv.setAttribute('class', 'bookDiv');
+    const title = document.createElement('p');
+    title.textContent = book.title;
+    const author = document.createElement('p');
+    author.textContent = book.author;
+    const delBtn = document.createElement('button');
+    delBtn.setAttribute('id', book.title);
+    delBtn.textContent = 'Remove';
+    delBtn.setAttribute('class', 'delete');
+    const hr = document.createElement('hr');
+    bookDiv.append(title, author, delBtn, hr);
+    return bookDiv;
 };
 
-const showcollection = () => {
-  const bookList = document.querySelector('#collection-list');
-  const collectionDiv = document.createElement('div', 'hr');
-  collection.forEach((book) => {
-    collectionDiv.appendChild(createBookDiv(book));
-  });
-  bookList.innerHTML = '';
-  bookList.appendChild(collectionDiv);
+const showBooks = () => {
+    const bookList = document.querySelector('#collection-list');
+    const booksDiv = document.createElement('div');
+    Book.books.forEach((book) => {
+        booksDiv.appendChild(createBookDiv(book));
+    });
+    bookList.innerHTML = '';
+    bookList.appendChild(booksDiv);
 };
 
 const clearInputs = () => {
-  document.querySelector('#title').value = '';
-  document.querySelector('#author').value = '';
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
 };
 
 document.querySelector('#book-form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  add(title, author);
-  showcollection();
-  clearInputs();
+    event.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const book = new Book(title, author);
+    Book.addBook(book);
+    showBooks();
+    clearInputs();
 });
 
 document.querySelector('#collection-list').onclick = (event) => {
-  if (event.target.className === 'delete') {
-    const bookDiv = event.target.closest('.bookDiv');
-    const { id } = bookDiv.querySelector('.delete');
-    remove(id);
-    bookDiv.remove();
-  }
+    if (event.target.className === 'delete') {
+        const bookDiv = event.target.closest('.bookDiv');
+        const { id } = bookDiv.querySelector('.delete');
+        Book.removeBook(id);
+        bookDiv.remove();
+    }
 };
-document.addEventListener('DOMContentLoaded', showcollection);
+
+document.addEventListener('DOMContentLoaded', showBooks);
